734692fec408664611e50ed5bee1ee92a13645f9
R0lk0 <116081448+R0lk0@users.noreply.github.com>
2024-09-17 11:34:14 +0200

    asd

---
+++ harommal oszthato szamok/harommal oszthato szamok/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harommal_oszthato_szamok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hárommal osztható számok 1-től 20-ig.");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            if (szam <= 1 || szam <= 20)
+            {
+                if (szam % 3 == 0)
+                {
+                    Console.WriteLine("A szám osztható hárommal.");
+                }
+                else
+                {
+                    Console.WriteLine("A szám nem osztható hárommal. ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("A szám nem felel meg az értékhatároknak.");
+            }
+        }
+    }
+}
